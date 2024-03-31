c75f45c4addfbc898c12b5e60357866f16a47024
ridabeepri <163827967+ridabeepri@users.noreply.github.com>
2024-03-31 15:31:08 -0400

    Update 04_tabular_analysis.R
    added package webshot2, changed data1 to data, table1 to table and gg function with save

--- code/04_tabular_analysis.R
+++ code/04_tabular_analysis.R
@@ -4,7 +4,7 @@ data <- readRDS(
   file = here::here("output/data.rds")
 )
 # loading packages
-pacman::p_load(dplyr, gt, labelled)
+pacman::p_load(dplyr, gt, labelled, webshot2)
 
 # Defining teams by conference 
 east <- c("BOS", "BRK", "NYK", "PHI", "TOR", "CHI", "CLE", "DET", "IND", "MIL", "ATL", "MIA", "ORL", "WAS", "CHO")
@@ -14,14 +14,14 @@ data$conference <- ifelse(data$team %in% east, "East",
                        ifelse(data$team %in% west, "West", NA))
 
 # counting pointss and rebounds for 36 mins
-data1 <- data %>%
+data <- data %>%
   mutate(
     pts_per_36 = (pts / mins_played) * 36,
     reb_per_36 = (rebounds_total / mins_played) * 36
   )
 
 # table label 
-table1 <- data1 %>%
+table <- data %>%
   group_by(conference) %>%
   summarise(
     Avg_Points_Per_36 = round(mean(pts_per_36, na.rm = TRUE), 2),
@@ -45,4 +45,4 @@ table1 <- data1 %>%
     decimals = 2
   )
 # Saving
-gtsave(filename = here::here("output", "table.png"))
+gg::gtsave(table, filename = here::here("output", "table.png"))
